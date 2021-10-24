22d1492075b82a39bf5c704025de12bd5be322b0
Harbans Singh <harbans.tech@gmail.com>
2021-10-24 23:51:37 +0530

    Smallest array to be sorted to sort whole array program

---
+++ Arrays & Vectors/Subarray_Sort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+pair<int, int> subarray(vector<int> vec, int n) {
+    int largest = INT_MIN;
+    int smallest = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        int x = vec[i];
+        if(i == 0 && x > vec[1]) {
+            largest = max(x, vec[i]);
+            smallest = min(x, vec[i]);
+        }
+        if(i = n-1 && x < vec[n-2]) {
+            largest = max(x, vec[i]);
+            smallest = min(x, vec[i]);
+        }
+        if(x > vec[i+1] || x < vec[i-1]) {
+            largest = max(x, vec[i]);
+            smallest = min(x, vec[i]);
+        }
+    }
+    if(largest = INT_MIN) {
+        return {0, 0};
+    }
+    int left_index {0};
+    int right_index {n-1};
+    while(smallest > vec[left_index]) {
+        left_index++;
+    }
+    while(largest < vec[right_index]) {
+        right_index--;
+    }
+    return {left_index, right_index};
+}
+
+//1 2 3 4 5 8 6 7 9 10 11
+int main() {
+    int n;
+    cout << "Enter length of array: ";
+    cin >> n;
+    vector<int> vec;
+    for(int i = 0; i < n; i++) {
+        int element;
+        cin >> element;
+        vec.push_back(element);
+    }
+    pair<int, int> result = subarray(vec, n);
+    cout << result.first << " " << result.second << endl;
+    return 0;
+}
