db89f306c86f9f3cc6886169d1be62a9cd665195
Harbans Singh <harbans.tech@gmail.com>
2021-10-25 13:32:07 +0530

    Subsorted array

--- Arrays & Vectors/Subarray_Sort.cpp
+++ Arrays & Vectors/Subarray_Sort.cpp
@@ -9,27 +9,27 @@ pair<int, int> subarray(vector<int> vec, int n) {
     for(int i = 0; i < n; i++) {
         int x = vec[i];
         if(i == 0 && x > vec[1]) {
-            largest = max(x, vec[i]);
-            smallest = min(x, vec[i]);
+            largest = max(x, largest);
+            smallest = min(x, smallest);
         }
-        if(i = n-1 && x < vec[n-2]) {
-            largest = max(x, vec[i]);
-            smallest = min(x, vec[i]);
+        if(i == n-1 && x < vec[n-2]) {
+            largest = max(x, largest);
+            smallest = min(x, smallest);
         }
         if(x > vec[i+1] || x < vec[i-1]) {
-            largest = max(x, vec[i]);
-            smallest = min(x, vec[i]);
+            largest = max(x, largest);
+            smallest = min(x, smallest);
         }
     }
-    if(largest = INT_MIN) {
+    if(smallest = INT_MAX) {
         return {0, 0};
     }
     int left_index {0};
     int right_index {n-1};
-    while(smallest > vec[left_index]) {
+    while(smallest >= vec[left_index]) {
         left_index++;
     }
-    while(largest < vec[right_index]) {
+    while(largest <= vec[right_index]) {
         right_index--;
     }
     return {left_index, right_index};
